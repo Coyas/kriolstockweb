7e4f7227faf892371ca90a22d206742280ca9a60
Ailton Duarte <adidas.coyas@gmail.com>
2021-01-21 21:07:57 -0100

    fix card link

--- components/Card/index.js
+++ components/Card/index.js
@@ -4,29 +4,29 @@ const Card = () => {
 	return (
 		<div class="p-2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
 			{/* <!--Card 1--> */}
-			<CardItem img="/DB/a.jpg" id="1" nome="dqsdqsdd" />
+			<CardItem img="/DB/a.jpg" id="a" nome="dqsdqsdd" />
 			{/* <!--Card 2--> */}
-			<CardItem img="/DB/b.jpg" id="2" nome="dqsdqsdd" />
+			<CardItem img="/DB/b.jpg" id="b" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/c.jpg" id="3" nome="dqsdqsdd" />
+			<CardItem img="/DB/c.jpg" id="c" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/d.jpg" id="4" nome="dqsdqsdd" />
+			<CardItem img="/DB/d.jpg" id="d" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/e.jpg" id="5" nome="dqsdqsdd" />
+			<CardItem img="/DB/e.jpg" id="e" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/f.jpg" id="6" nome="dqsdqsdd" />
+			<CardItem img="/DB/f.jpg" id="f" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/g.jpg" id="7" nome="dqsdqsdd" />
+			<CardItem img="/DB/g.jpg" id="g" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/h.jpg" id="8" nome="dqsdqsdd" />
+			<CardItem img="/DB/h.jpg" id="h" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/i.jpg" id="9" nome="dqsdqsdd" />
+			<CardItem img="/DB/i.jpg" id="i" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/j.jpg" id="9" nome="dqsdqsdd" />
+			<CardItem img="/DB/j.jpg" id="j" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/k.jpg" id="9" nome="dqsdqsdd" />
+			<CardItem img="/DB/k.jpg" id="k" nome="dqsdqsdd" />
 			{/* <!--Card 3--> */}
-			<CardItem img="/DB/l.jpg" id="9" nome="dqsdqsdd" />
+			<CardItem img="/DB/l.jpg" id="l" nome="dqsdqsdd" />
 		</div>
 	);
 };
