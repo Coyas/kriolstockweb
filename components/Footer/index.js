3079e21670f0d24ef70d9f6a38827512ef8f8f53
Ailton Duarte <adidas.coyas@gmail.com>
2021-01-21 16:55:35 -0100

    add logo & change colors

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,65 @@
+// &copy; kriolStock 2021 by TerraSsytem
+
 const Footer = () => {
-	return <p>footer</p>;
+	return (
+		<footer className="bg-gray-800">
+			{/* <div>alguns dados para se por no footer</div>
+			<p className="divide-y divide-white">terrs</p> */}
+			<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+				<div className="relative flex items-center justify-between h-16">
+					<a className="text-gray-400">&copy; KriolStock 2021</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Termos
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Privacidade
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Ajuda
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						<img src="/kriolStockLogo2.png" width="100" />
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						preços
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Blog
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Sobre
+					</a>
+					<a
+						href="#"
+						className="text-gray-300 hover:text-white hover:underline"
+					>
+						Contribua
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -37,13 +37,13 @@ const NavBar = () => {
 
 	return (
 		// <!-- This example requires Tailwind CSS v2.0+ -->
-		<nav className="bg-gray-800">
+		<nav className="bg-blue-600">
 			<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
 				<div className="relative flex items-center justify-between h-16">
 					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 						{/* <!-- Mobile menu button--> */}
 						<button
-							className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+							className="inline-flex items-center justify-center p-2 rounded-md text-gray-100 hover:text-white hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 							aria-expanded="false"
 							onClick={MvisivelClick}
 						>
@@ -94,7 +94,7 @@ const NavBar = () => {
 					</div>
 					<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
 						<div className="flex-shrink-0 flex items-center">
-							<img
+							{/* <img
 								className="block lg:hidden h-8 w-auto"
 								src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
 								alt="Workflow"
@@ -103,38 +103,48 @@ const NavBar = () => {
 								className="hidden lg:block h-8 w-auto"
 								src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
 								alt="Workflow"
+							/> */}
+							<img
+								className="block lg:hidden h-8 w-auto"
+								src="/logo.png"
+								alt="kriolStocklogo"
+							/>
+							<img
+								className="hidden lg:block h-8 w-auto"
+								src="/logo.png"
+								alt="kriolStocklogo"
 							/>
 						</div>
 						<div className="hidden sm:block sm:ml-6">
 							<div className="flex space-x-4">
-								{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+								{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-blue-400 hover:text-white" --> */}
 								<a
 									href="/"
-									className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+									className="bg-blue-400 text-white px-3 py-2 rounded-md text-sm font-medium"
 								>
 									Home
 								</a>
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+									className="text-gray-100 hover:bg-blue-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 								>
 									Imagens
 								</a>
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+									className="text-gray-100 hover:bg-blue-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 								>
 									Ilustraçoes
 								</a>
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+									className="text-gray-100 hover:bg-blue-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 								>
 									icons
 								</a>
 								<a
 									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+									className="text-gray-100 hover:bg-blue-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
 								>
 									Videos
 								</a>
@@ -142,7 +152,7 @@ const NavBar = () => {
 						</div>
 					</div>
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-						<button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+						<button className="bg-blue-400 p-1 rounded-full text-gray-100 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-white">
 							<span className="sr-only">View notifications</span>
 							{/* <!-- Heroicon name: bell --> */}
 							<svg
@@ -166,7 +176,7 @@ const NavBar = () => {
 						<div className="ml-3 relative">
 							<div>
 								<button
-									className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+									className="bg-blue-400 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-white"
 									id="user-menu"
 									aria-haspopup="true"
 									onClick={UvisivelClick}
@@ -174,7 +184,7 @@ const NavBar = () => {
 									<span className="sr-only">Open user menu</span>
 									<img
 										className="h-8 w-8 rounded-full"
-										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+										src="/userIcon.png"
 										alt=""
 									/>
 								</button>
@@ -231,34 +241,34 @@ const NavBar = () => {
   --> */}
 			<div className={`${mvisivel ? "hidden sm:hidden" : "block"}`}>
 				<div className="px-2 pt-2 pb-3 space-y-1">
-					{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+					{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-100 hover:bg-blue-400 hover:text-white" --> */}
 					<a
 						href="#"
-						className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+						className="bg-blue-400 text-white block px-3 py-2 rounded-md text-base font-medium"
 					>
 						Home
 					</a>
 					<a
 						href="#"
-						className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						className="text-gray-100 hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 					>
 						Imagens
 					</a>
 					<a
 						href="#"
-						className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						className="text-gray-100 hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 					>
 						Ilustraçoes
 					</a>
 					<a
 						href="#"
-						className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						className="text-gray-100 hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 					>
 						Icons
 					</a>
 					<a
 						href="#"
-						className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+						className="text-gray-100 hover:bg-blue-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
 					>
 						Videos
 					</a>
