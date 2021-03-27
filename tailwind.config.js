52dee890c8c381eec23f7285ecc96c7996a866d1
Ailton Duarte <adidas.coyas@gmail.com>
2021-03-27 16:33:39 -0100

    add google fonts components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+// const colors = require("tailwindcss/colors");
+
 module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		// colors: {
+		// cinza100: "#4b5563", //seccoes
+		// cinza200: "#374151",// letras
+		// azul100: "#00284b",
+		// vermelho100: colors.red.600,
+		// vermelho200: colors.red.500,
+		// vermelho300: colors.red.400,
+		// branco: "#ffffff",
+		// amarelo100: colors.yellow.600,
+		// amarelo200: colors.yellow.500,
+		// amarelo300: colors.yellow.400,
+		// },
 		extend: {},
 	},
 	variants: {
