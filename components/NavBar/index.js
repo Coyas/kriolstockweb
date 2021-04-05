e710fb1b93211b76c4bc2477b52385ed62734a91
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-05 16:06:29 -0100

    change color of x to black

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -86,7 +86,7 @@ const NavBar = () => {
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
-								stroke="currentColor"
+								stroke="black"
 								aria-hidden="true"
 							>
 								<path
