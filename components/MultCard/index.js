aa31165804cda02e2a5e9a639dcecad0009794bc
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-06 00:51:27 -0100

    remove extra div

--- components/MultCard/index.js
+++ components/MultCard/index.js
@@ -2,34 +2,32 @@ import CardItens from "../CardItens";
 
 const MultCard = () => {
 	return (
-		<div>
-			<div className="mt-8 grid lg:grid-cols-4 gap-3">
-				{/* <!-- cards go here --> */}
-				<CardItens
-					id="a"
-					link="/foto1.png"
-					author="kriolStock"
-					title="latada gammers"
-				/>
-				<CardItens
-					id="b"
-					link="/DB/b.jpg"
-					author="kalua filmes"
-					title="oasis na brava"
-				/>
-				<CardItens
-					id="c"
-					link="/DB/c.jpg"
-					author="SCI"
-					title="pequenos sorisos"
-				/>
-				<CardItens
-					id="c"
-					link="/DB/d.jpg"
-					author="PicVideos"
-					title="bera mar e sem areia"
-				/>
-			</div>
+		<div className="grid lg:grid-cols-4 gap-3">
+			{/* <!-- cards go here --> */}
+			<CardItens
+				id="a"
+				link="/foto1.png"
+				author="kriolStock"
+				title="latada gammers"
+			/>
+			<CardItens
+				id="b"
+				link="/foto-site.png"
+				author="kalua filmes"
+				title="oasis na brava"
+			/>
+			<CardItens
+				id="c"
+				link="/DB/c.jpg"
+				author="SCI"
+				title="pequenos sorisos"
+			/>
+			<CardItens
+				id="c"
+				link="/DB/d.jpg"
+				author="PicVideos"
+				title="bera mar e sem areia"
+			/>
 		</div>
 	);
 };
