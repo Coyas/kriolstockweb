bd51d41203b531b10c3139a221b857e319e07734
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-13 00:44:39 -0100

    login features

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -9,7 +9,7 @@ const Login = () => {
 					style={{ filter: "blur(6px)", backgroundImage: "url('/login.jpg')" }}
 				></div>
 				<div
-					className="absolute w-3/5 bg-white"
+					className="absolute w-auto bg-white"
 					style={{
 						transform: "translate(-50%, -50%)",
 						top: "50%",
@@ -23,13 +23,13 @@ const Login = () => {
 						/>
 					</div>
 					<div className="px-12 py-10">
-						<div className="w-full mb-2">
+						<div className="w-full mb-4">
 							<div className="flex items-center">
 								<i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user"></i>
 								<input
 									type="text"
-									placeholder="username"
-									className="-mx-6 px-8  w-full border rounded px-3 py-1 text-gray-700"
+									placeholder="Username ou E-mail"
+									className="-mx-6 px-8  w-full border rounded py-1 text-gray-700"
 								/>
 							</div>
 						</div>
@@ -38,10 +38,14 @@ const Login = () => {
 								<i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock"></i>
 								<input
 									type="text"
-									placeholder="password"
-									className="-mx-6 px-8 w-full border rounded px-3 py-1 text-gray-700"
+									placeholder="Password"
+									className="-mx-6 px-8 w-full border rounded py-1 text-gray-700"
 								/>
 							</div>
+						</div>
+						<div className="flex justify-between mt-4 text-xs">
+							<a href="/auth/forgotpasswd">Esqueceu a senha?</a>
+							<a href="/auth/signup">Ainda nao tem conta?</a>
 						</div>
 						<div className="mt-8 flex justify-between">
 							<div className="flex items-center">
@@ -50,11 +54,29 @@ const Login = () => {
 							</div>
 							<div>
 								<button
+									type="submit"
 									type="text"
-									className="bg-yellow-400 text-xs text-gray-700 rounded px-4 py-2"
+									className="bg-white font-bold hover:bg-red-700 border border-red-700 text-xs text-gray-700 hover:text-white rounded px-4 py-2"
 								>
-									SIGN IN
+									Entrar
 								</button>
+							</div>
+						</div>
+						<div className=""></div>
+						<div className="mt-5">
+							<div className="flex flex-row justify-between">
+								<a
+									type="text"
+									className="bg-blue-400 text-md text-white rounded px-6 py-3 font-bold"
+								>
+									Facebook
+								</a>
+								<a
+									type="text"
+									className="bg-red-400 text-white text-md  rounded px-8 py-3 font-bold"
+								>
+									Google
+								</a>
 							</div>
 						</div>
 					</div>
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,5 +1,84 @@
+import Authlayout from "../../components/Layout/Authlayout";
+
 const signup = () => {
-	return <div>signup</div>;
+	return (
+		<Authlayout>
+			<div style={{ fontFamily: "Roboto" }}>
+				<div
+					className="w-full h-screen md:-mx-4"
+					style={{ filter: "blur(6px)", backgroundImage: "url('/login.jpg')" }}
+				></div>
+				<div
+					className="absolute w-auto bg-white"
+					style={{
+						transform: "translate(-50%, -50%)",
+						top: "50%",
+						left: "50%",
+					}}
+				>
+					<div className="flex justify-center -mt-10">
+						<img
+							className="border-2 w-20 h-20 rounded-full"
+							src="/kriolfavicon.png"
+						/>
+					</div>
+					<form className="px-12 py-10">
+						<div className="w-full mb-2">
+							<div className="flex items-center">
+								<i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-user"></i>
+								<input
+									type="text"
+									placeholder="Username ou E-mail"
+									className="-mx-6 px-8  w-full border rounded py-1 text-gray-700"
+								/>
+							</div>
+						</div>
+						<div className="w-full mb-2">
+							<div className="flex items-center">
+								<i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock"></i>
+								<input
+									type="text"
+									placeholder="Password"
+									className="-mx-6 px-8 w-full border rounded py-1 text-gray-700"
+								/>
+							</div>
+						</div>
+
+						<div className="w-full mb-4">
+							<div className="flex items-center">
+								<i className="ml-3 fill-current text-gray-400 text-xs z-10 fas fa-lock"></i>
+								<input
+									type="text"
+									placeholder="repetir Password"
+									className="-mx-6 px-8 w-full border rounded py-1 text-gray-700"
+								/>
+							</div>
+						</div>
+
+						<div className="mt-8 flex justify-between">
+							<div>
+								<a
+									href="/"
+									className="bg-white font-bold hover:bg-gray-400 border border-gray-400 text-xs text-gray-700 hover:text-white rounded px-4 py-2"
+								>
+									Cancelar
+								</a>
+							</div>
+							<div>
+								<button
+									type="submit"
+									className="bg-white font-bold hover:bg-red-700 border border-red-700 text-xs text-gray-700 hover:text-white rounded px-4 py-2"
+								>
+									Registrar
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</Authlayout>
+		// </div>
+	);
 };
 
 export default signup;
