866f295b1348c36b41fc34e868492d71af0f3a0b
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-13 01:49:51 -0100

    create search page

--- pages/imagens/index.js
+++ pages/imagens/index.js
@@ -1,9 +1,154 @@
 import Layout from "../../components/Layout";
+import CardItens from "../../components/CardItens";
+
+<CardItens
+	id="a"
+	link="/foto1.png"
+	author="kriolStock"
+	title="latada gammers"
+/>;
 
 const Imagens = () => {
 	return (
 		<Layout>
-			<p>imagens</p>
+			<div className="grid grid-cols-4">
+				<div className="bg-red-700 border-r border-gray-400 flex flex-col p-5">
+					<div className="bg-white">
+						<div>dsdf</div>
+					</div>
+					<div className="bg-white">
+						<div>dsdf</div>
+					</div>
+					<div className="bg-white">
+						<div>dsdf</div>
+					</div>
+					<div className="bg-white">
+						<div>dsdf</div>
+					</div>
+				</div>
+				<div className="bg-yellow-600 col-span-3 grid lg:grid-cols-4 gap-1">
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+					<CardItens
+						id="a"
+						link="/foto1.png"
+						author="kriolStock"
+						title="latada gammers"
+					/>
+				</div>
+			</div>
 		</Layout>
 	);
 };
