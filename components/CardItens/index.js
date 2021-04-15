8fa215b2a318db0b5155cce15c18e237df180112
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-15 16:45:35 -0100

    add llinks & data

--- components/CardItens/index.js
+++ components/CardItens/index.js
@@ -1,16 +1,14 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 
 const CardItens = (props) => {
 	// alert(props.link);
 	return (
 		// <!-- component -->
-		// https://images.pexels.com/photos/3738673/pexels-photo-3738673.jpeg?auto=compress&cs=tinysrgb&h=350
 		<div
 			className="bg-gray-100 m-auto w-full h-64"
 			style={{
-				backgroundImage:
-					"url('https://images.pexels.com/photos/3738673/pexels-photo-3738673.jpeg?auto=compress&cs=tinysrgb&h=350')",
+				backgroundImage: "url('" + props.link + "')",
 				backgroundPosition: "center",
 				backgroundRepeat: "no-repeat",
 				backgroundSize: "cover",
@@ -18,13 +16,21 @@ const CardItens = (props) => {
 		>
 			<div className="flex flex-row items-end h-full w-full">
 				<div className="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
-					<h3 className="text-base font-bold leading-5 uppercase">
-						Lorem, ipsum dolor sit amet elit foure consectetur adipisicing.
-					</h3>
+					<Link href={`/details/${props.id}`}>
+						<a>
+							<h3 className="text-base font-bold leading-5 uppercase">
+								{props.title}
+							</h3>
+						</a>
+					</Link>
 					<div className="inline-flex items-center">
-						<span className="capitalize font-base text-xs my-1 mr-1">
-							Agnezmo Tuginem
-						</span>
+						<Link href={`/me/${props.author}`}>
+							<a>
+								<span className="capitalize font-base text-xs my-1 mr-1">
+									{props.author}
+								</span>
+							</a>
+						</Link>
 						<svg
 							className="stroke-current stroke-1 text-blue-600 w-4"
 							xmlns="http://www.w3.org/2000/svg"
@@ -41,38 +47,46 @@ const CardItens = (props) => {
 					<div className="flex flex-row justify-between">
 						<div className="flex flex-row">
 							<div className="w-max inline-flex items-center">
-								<svg
-									className="w-4"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-									/>
-								</svg>
-								<span className="text-xs ml-1 antialiased">0</span>
+								<Link href={`/me/${props.id}/likey`}>
+									<a>
+										<svg
+											className="w-4"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+											/>
+										</svg>
+									</a>
+								</Link>
+								<span className="text-xs ml-1 antialiased">5</span>
 							</div>
 							<div className="w-max inline-flex ml-4 items-center">
-								<svg
-									className="w-4"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-									/>
-								</svg>
-								<span className="text-xs ml-1 antialiased">1</span>
+								<Link href={`/me/${props.id}/comenta`}>
+									<a>
+										<svg
+											className="w-4"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+											/>
+										</svg>
+									</a>
+								</Link>
+								<span className="text-xs ml-1 antialiased">0</span>
 							</div>
 							<div className="w-max inline-flex ml-4 items-center">
 								<svg
