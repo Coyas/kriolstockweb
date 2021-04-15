e72819e15ce6db90993ed28e127d9d567ff11c82
Ailton Duarte <adidas.coyas@gmail.com>
2021-04-15 16:45:11 -0100

    create image db

--- components/MultCard/index.js
+++ components/MultCard/index.js
@@ -1,33 +1,50 @@
 import CardItens from "../CardItens";
 
 const MultCard = () => {
+	const images = [
+		{
+			id: "a",
+			link:
+				"https://res.cloudinary.com/coyas/image/upload/v1618507062/site/kriol/BoaVista_f7rvab.jpg",
+			author: "kriolstock",
+			title: "Praia de Santa Monica,Boavista",
+		},
+		{
+			id: "b",
+			link:
+				"https://res.cloudinary.com/coyas/image/upload/v1618507059/site/kriol/BoaVistab_epxham.jpg",
+			author: "kriolstock",
+			title: "Deserto de Viana, BoaVista",
+		},
+		{
+			id: "c",
+			link:
+				"https://res.cloudinary.com/coyas/image/upload/v1618507055/site/kriol/BoaVistac_cjrtv8.jpg",
+			author: "kriolstock",
+			title: "Noite No Deserto de Viana, BoaVista",
+		},
+		{
+			id: "d",
+			link:
+				"https://res.cloudinary.com/coyas/image/upload/v1618507067/site/kriol/fogo_k9mlaf.jpg",
+			author: "kriolstock",
+			title: "Vulcao do Fogo",
+		},
+	];
+
+	console.log(images);
 	return (
 		<div className="grid lg:grid-cols-4 gap-3">
 			{/* <!-- cards go here --> */}
-			<CardItens
-				id="a"
-				link="/foto1.png"
-				author="kriolStock"
-				title="latada gammers"
-			/>
-			<CardItens
-				id="b"
-				link="/foto-site.png"
-				author="kalua filmes"
-				title="oasis na brava"
-			/>
-			<CardItens
-				id="c"
-				link="/DB/c.jpg"
-				author="SCI"
-				title="pequenos sorisos"
-			/>
-			<CardItens
-				id="c"
-				link="/DB/d.jpg"
-				author="PicVideos"
-				title="bera mar e sem areia"
-			/>
+			{images &&
+				images.map((value, index) => (
+					<CardItens
+						id={`${value.id}${index}`}
+						link={value.link}
+						author={value.author}
+						title={value.title}
+					/>
+				))}
 		</div>
 	);
 };
